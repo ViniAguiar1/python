9ea4be9623e8d22d8e95f335094e269ac25d10bc
viniaguiar20 <88693741+viniaguiar20@users.noreply.github.com>
2023-02-17 15:08:31 -0300

    Att

---
+++ introducao-variaveis/aula7.py
@@ -0,0 +1,10 @@
+# nome_completo = "Viinicius Aguiar"
+# soma_dois_mais_dois = 2 + 2
+# int_um = int("1")
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = "Vinicius"
+idade = 19
+maior_de_idade = idade >= 18
+print("Nome:", nome)
+print('É maior? ', maior_de_idade)
